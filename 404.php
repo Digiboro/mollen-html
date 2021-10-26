0379d2997d20e43e18f045acb3ccaf9e7d230d94
Danil <goodproject01@mail.ru>
2021-10-26 08:46:14 +0300

    day 1

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <?php include 'include-blocks/head.php' ?>
+  <title>Mollen</title>
+</head>
+
+<body>
+  <main class="page-404 main">
+    <div class="page-404-inner">
+      <h1 class="page-404-title">404</h1>
+      <p class="page-404-text">страница не найдена или была удалена</p>
+      <a class="btn btn-large" href="">
+        <span class="btn-text">Перейти в каталог</span>
+      </a>
+    </div>
+  </main>
+
+  <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ faq.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <?php include 'include-blocks/head.php' ?>
+  <title>Mollen</title>
+</head>
+
+<body>
+  <!-- header -->
+  <?php include 'include-blocks/header.php' ?>
+
+  <main class="page-faq main">
+    <div class="container">
+      <div class="page-faq-inner">
+        <div class="breadcrumbs">
+          <ul class="breadcrumbs-list">
+            <li class="breadcrumbs-item">
+              <a class="breadcrumbs-link" href="">Главная</a>
+            </li>
+            <li class="breadcrumbs-item">
+              <a class="breadcrumbs-link" href="">Главная</a>
+            </li>
+            <li class="breadcrumbs-item">
+              Оформление заказа
+            </li>
+          </ul>
+        </div>
+
+        <h2 class="faq-title">Часто задаваемые вопросы</h2>
+
+        <div class="faq">
+          <div class="faq-item">
+            <div class="faq-item-top">
+              <span class="faq-item-title">Как узнать размеры? Какие параметры в размере евро/двуспальный и тд?</span>
+              <button class="faq-item-icon"><svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 6.7385v-1.477h5.275V0h1.4808v5.2615H12v1.477H6.7558V12H5.275V6.7385H0Z" fill="#72716E"/></svg></button>
+            </div>
+            <div class="faq-item-content">
+              <p class="faq-item-text">Текст</p>
+            </div>
+          </div>
+
+          <div class="faq-item">
+            <div class="faq-item-top">
+              <span class="faq-item-title">Где вы находитесь?</span>
+              <button class="faq-item-icon"><svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 6.7385v-1.477h5.275V0h1.4808v5.2615H12v1.477H6.7558V12H5.275V6.7385H0Z" fill="#72716E"/></svg></button>
+            </div>
+            <div class="faq-item-content">
+              <p class="faq-item-text">Текст</p>
+            </div>
+          </div>
+
+          <div class="faq-item">
+            <div class="faq-item-top">
+              <span class="faq-item-title">Сколько занимает пошив?</span>
+              <button class="faq-item-icon"><svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 6.7385v-1.477h5.275V0h1.4808v5.2615H12v1.477H6.7558V12H5.275V6.7385H0Z" fill="#72716E"/></svg></button>
+            </div>
+            <div class="faq-item-content">
+              <p class="faq-item-text">Текст</p>
+            </div>
+          </div>
+
+          <div class="faq-item">
+            <div class="faq-item-top">
+              <span class="faq-item-title">Как определить какой размер мне подходит?</span>
+              <button class="faq-item-icon"><svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 6.7385v-1.477h5.275V0h1.4808v5.2615H12v1.477H6.7558V12H5.275V6.7385H0Z" fill="#72716E"/></svg></button>
+            </div>
+            <div class="faq-item-content">
+              <p class="faq-item-text">Текст</p>
+            </div>
+          </div>
+
+          <div class="faq-item active">
+            <div class="faq-item-top">
+              <span class="faq-item-title">Если мне не понравится товар, что делать?</span>
+              <button class="faq-item-icon"><svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 6.7385v-1.477h5.275V0h1.4808v5.2615H12v1.477H6.7558V12H5.275V6.7385H0Z" fill="#72716E"/></svg></button>
+            </div>
+            <div class="faq-item-content">
+              <p class="faq-item-text">Экспресс-отправления EMS — это самый быстрый и удобный способ доставить письмо или посылку по России и за границу. Курьер заберет отправление в удобном для вас месте и доставит его адресату домой или в офис. Экспресс-отправление является регистрируемым, его доставку и вручение можно отследить с помощью трек-номера. В городах, где нет курьерской службы EMS, отправить и получить экспресс-отправление можно через отделение Почты России</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <!-- footer -->
+  <?php include 'include-blocks/footer.php' ?>
+
+  <script src="js/main.js"></script>
+</body>
+
+</html>
